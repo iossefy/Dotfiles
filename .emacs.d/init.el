f7dd5044863058135c46a78db76592654800e0e6
M1cR0xf7 <git@youssefy.com>
2022-03-07 00:05:14 +0200

    [emacs] add move-line-{up, down} functions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -80,6 +80,23 @@
 ;; escape
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+;;;; editor
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
 
 ;; set font
 ;; WARNING: You should have Fira Code font installed
