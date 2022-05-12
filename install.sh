d0a17ebe5c022e664bc11513abc31b434a3b86cd
M1cR0xf7 <git@youssefy.com>
2022-05-12 19:22:33 +0200

    [install/emacs] get emacs's leyl theme and put it in emacs directory

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# copy dotfiles from this directory
+# to the directory it's supposed to be
 cp -rf ./.scripts/ ~/
 cp -rf ./.emacs.d/ ~/
 cp ./.xinitrc      ~/.xinitrc
@@ -7,5 +9,10 @@ cp ./.fehbg        ~/.fehbg
 cp ./.tmux.conf    ~/.tmux.conf
 cp ./.vimrc        ~/.vimrc
 cp .zshrc          ~/.zshrc
+
+# install leyl theme into emacs
+git clone https://github.com/m1cr0xf7/leyl
+cp leyl/leyl-theme.el ~/.emacs.d/themes/
+
 
 echo "Done."
